53205dbbf56628e18375a8429895b6ad8bcd846e
shemogumbe <shem.ogumbe@andela.com>
2018-09-05 00:02:31 +0300

    feat(set up): create serve instance

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Welcome to HealthChecks");
+/*
+* Server instance file
+*/
+
+const http = require('http');
+
+// create a server instance
+const server = http.createServer(function(request, response){
+    response.end("Welcome To Health checks");
+})
+
+// serve and listen on a port
+server.listen(3000, function(){
+    console.log("The server is listening on port 3000!");
+})
