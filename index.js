94b5c8d07e9865d99528142f72c884a2b88440a9
shemogumbe <shem.ogumbe@andela.com>
2018-09-05 00:32:26 +0300

    feat(set up): get requested paths

--- index.js
+++ index.js
@@ -3,10 +3,26 @@
 */
 
 const http = require('http');
+const url = require('url');
 
 // create a server instance
 const server = http.createServer(function(request, response){
+    
+    // get the Url from the request 
+    const parseedUrl = url.parse(request.url, true)
+
+    // get path of the requested resource
+    const path = parseedUrl.pathname
+
+    // trim the path
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+    console.log(trimmedPath)
+
     response.end("Welcome To Health checks");
+
+    
+    // log the requested path
+    console.log("The request received for " + trimmedPath)
 })
 
 // serve and listen on a port
